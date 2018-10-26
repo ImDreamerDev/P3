566a06397a93e2e9ff420c7cc0e91b972fdacee9
Unknown <mlindo17@student.aau.dk>
2018-10-26 13:46:12 +0200

    Test

--- src/main/java/dk/aau/ds304e18/math/Main.java
+++ src/main/java/dk/aau/ds304e18/math/Main.java
@@ -3,7 +3,7 @@ package dk.aau.ds304e18.math;
 public class Main {
     public static void main(String[] args) {
 
-        InverseGaussian invG = new InverseGaussian(1,1);
+        InverseGaussian invG = new InverseGaussian(11.03,66.661341);
 
         System.out.println(invG.getProbability(5));
     }
