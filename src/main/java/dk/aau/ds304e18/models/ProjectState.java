2a0f0466f1b4fc45271184822f427013efd1da34
Lasse Rasmussen <freedomforce360@gmail.com>
2018-10-29 15:06:46 +0100

    Added comment spaces for Kasper til fill out PepePlz

--- src/main/java/dk/aau/ds304e18/models/ProjectState.java
+++ src/main/java/dk/aau/ds304e18/models/ProjectState.java
@@ -1,17 +1,39 @@
 package dk.aau.ds304e18.models;
 
+/**
+ * The states that a given project can have.
+ */
 public enum ProjectState {
 
     /**
-     * The states that a given project can have.
+     *
      */
-    ONGOING(0), COMPLETED(1), ARCHIVED(2);
+    ONGOING(0),
+    /**
+     *
+     */
+    COMPLETED(1),
+    /**
+     *
+     */
+    ARCHIVED(2);
+
+    /**
+     *
+     */
     private final int value;
 
+    /**
+     * @param value
+     */
     ProjectState(int value) {
         this.value = value;
     }
 
+    /**
+     * 
+     * @return
+     */
     public int getValue() {
         return value;
     }
