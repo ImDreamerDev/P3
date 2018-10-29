cbff371e35f44d5d2e96c9d40a2d6a126e5d68d1
Søren <sc17@student.aau.dk>
2018-10-29 15:16:07 +0100

    Test written for getValue in ProjectState

---
+++ src/test/java/ds304e18/models/ProjectStateTest.java
@@ -0,0 +1,25 @@
+package ds304e18.models;
+
+import dk.aau.ds304e18.models.ProjectState;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ProjectStateTest {
+
+    @Test
+    void TestProjectStateGetValue01() {
+        assertEquals(0,ProjectState.ONGOING.getValue());
+    }
+
+    @Test
+    void TestProjectStateGetValue02() {
+        assertEquals(1,ProjectState.COMPLETED.getValue());
+    }
+
+    @Test
+    void TestProjectStateGetValue03() {
+        assertEquals(2,ProjectState.ARCHIVED.getValue());
+    }
+
+}
