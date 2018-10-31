4970a6fad39c880d879fd27581f90c0ba6e37a5d
Rasmus Smit <rasmus-sl@hotmail.com>
2018-10-31 12:18:30 +0100

    Added one more test for DB dab :+1:

--- src/test/java/ds304e18/database/DatabaseManagerTest.java
+++ src/test/java/ds304e18/database/DatabaseManagerTest.java
@@ -1,22 +1,57 @@
 package ds304e18.database;
 
 import dk.aau.ds304e18.database.DatabaseManager;
+import dk.aau.ds304e18.models.Employee;
+import dk.aau.ds304e18.models.Project;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.postgresql.util.PSQLException;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class DatabaseManagerTest {
 
+    @BeforeAll
+    static void init() {
+        DatabaseManager.isTests = true;
+    }
+
 
     /**
-     * Tests that the result from the employees table is not null.
+     * Asserts that addEmployee function in DatabaseManager works.
+     *
+     * @throws SQLException
      */
     @Test
-    void assertQuery() {
-        DatabaseManager.isTests = true;
-        ResultSet rs = DatabaseManager.query("SELECT * FROM employees");
+    void testAddEmployee() {
+        Employee testEmp = new Employee("Ur mum");
+
+        ResultSet rs = DatabaseManager.query("SELECT * FROM employees WHERE id = " + testEmp.getId());
         assertNotNull(rs);
+        try {
+            rs.next();
+            assertEquals(testEmp.getId(), rs.getInt(1));
+            assertEquals(testEmp.getName(), rs.getString(2));
+
+            DatabaseManager.query("DELETE FROM employees WHERE id = " + testEmp.getId());
+        } catch (SQLException ignored) {
+
+        }
     }
+
+    @Test
+    void testAddProject() throws SQLException {
+        Project testProj = new Project("TestProj");
+        ResultSet rs = DatabaseManager.query("SELECT * FROM projects WHERE id = " + testProj.getId());
+        assertNotNull(rs);
+        rs.next();
+        assertEquals(testProj.getId(), rs.getInt(1));
+        assertEquals(testProj.getName(), rs.getString(2));
+        DatabaseManager.query("DELETE FROM projects WHERE id = " + testProj.getId());
+    }
+
 }
