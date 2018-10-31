35e9ab724e1bcfdaa55e26ad0f756a0ae8513b0f
Milton <mlindo17@student.aau.dk>
2018-10-31 15:06:07 +0100

    Sequence now gives string
    Also tests has been redone

--- src/main/java/dk/aau/ds304e18/sequence/Sequence.java
+++ src/main/java/dk/aau/ds304e18/sequence/Sequence.java
@@ -8,13 +8,30 @@ import java.util.List;
 
 public class Sequence {
 
-    public static List<Task> sequenceTasks(List<Task> taskList) {
+    public static String sequenceTasks(List<Task> taskList) {
+
+        /*
+        | indicates where it's supposed to be drawn
+        taskId(taskId, taskId) indicates dependencies
+        taskIds are separated by a comma
+
+        ex:
+
+        1,2|3(1),4(1,2)|5(2,3),6(4)|7(5)
+
+        Would be that 1 and 2 can be done in parallel
+        3 has a dependency on 1
+        4 has a dependency on 1 and 2
+        5 has a dependency on 2 and 3
+        6 has a dependency on 4
+        7 has a dependency on 5
+         */
 
         //So we don't change the task list in the project
         List<Task> tasks = new ArrayList<>(taskList);
 
-        //The list to return
-        List<Task> sequencedTasks = new ArrayList<>();
+        //The sequence to return
+        StringBuilder sequencedTasks = new StringBuilder();
 
         //Temporary lists, first to sort them and add them to sequencedTasks, second to remove the already sequenced tasks from the dependency list of each task
         List<Task> tasksToSort = new ArrayList<>();
@@ -39,7 +56,19 @@ public class Sequence {
             tasksToRemove = new ArrayList<>();
 
             //Add the sorted tasks to the sequencedTasks list
-            sequencedTasks.addAll(sortTasks(tasksToSort));
+            List<Task> tasksToInsert = sortTasks(tasksToSort);
+            for(int i = 0; i < tasksToInsert.size(); i++){
+                if(i != tasksToInsert.size()-1){
+                    sequencedTasks.append(tasksToInsert.get(i).getId());
+                    sequencedTasks.append(appendDependencies(tasksToInsert.get(i)));
+                    sequencedTasks.append(",");
+                } else {
+                    sequencedTasks.append(tasksToInsert.get(i).getId());
+                    sequencedTasks.append(appendDependencies(tasksToInsert.get(i)));
+                    if(tasks.size() != 0)
+                        sequencedTasks.append("|");
+                }
+            }
             tasksToSort = new ArrayList<>();
 
             //For each task in the yet to be sorted list
@@ -49,7 +78,7 @@ public class Sequence {
                 for (Task dependency : task.getDependencies()) {
 
                     //If the dependency is already sequenced
-                    if (sequencedTasks.contains(dependency)) {
+                    if (!tasks.contains(dependency)) {
                         //Count down the amount of dependencies left so we know that if they're at 0, it's ready to be sequenced
                         task.setAmountDependenciesLeft(task.getAmountDependenciesLeft() - 1);
                     }
@@ -58,7 +87,7 @@ public class Sequence {
         }
 
         //Return the list of sequenced tasks
-        return sequencedTasks;
+        return sequencedTasks.toString();
     }
 
     private static List<Task> sortTasks(List<Task> tasks) {
@@ -68,6 +97,28 @@ public class Sequence {
 
         //Return the sorted tasks
         return tasks;
+
+    }
+
+    private static String appendDependencies(Task task) {
+
+        if(task.getDependencies().size() == 0) return "";
+
+        StringBuilder returnString = new StringBuilder();
+
+        returnString.append("(");
+
+        for(int i = 0; i < task.getDependencies().size(); i++){
+            if(i != task.getDependencies().size()-1){
+                returnString.append(task.getDependencies().get(i).getId()).append(",");
+            }else{
+                returnString.append(task.getDependencies().get(i).getId());
+            }
+        }
+
+        returnString.append(")");
+
+        return returnString.toString();
 
     }
 
