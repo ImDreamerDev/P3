59d9114d989a37ae3f52fc7699bffec242550f8f
Lasse Rasmussen <freedomforce360@gmail.com>
2018-11-07 13:04:32 +0100

    Input refactor PepeHands

--- src/main/java/dk/aau/ds304e18/ui/InputTab.java
+++ src/main/java/dk/aau/ds304e18/ui/InputTab.java
@@ -1,9 +1,4 @@
 package dk.aau.ds304e18.ui;
-/*
- * Author: Lasse Stig Emil Rasmussen
- * Email: lser17@student.aau.dk
- * Class: Software 2nd semester
- */
 
 import dk.aau.ds304e18.JavaFXMain;
 import dk.aau.ds304e18.LocalObjStorage;
@@ -32,6 +27,7 @@ public class InputTab {
     private List<Task> taskDependencies = new ArrayList<>();
     private Parent rootPane;
     private OutputTab outputTab;
+    private TableView<Task> tableView;
 
     public InputTab(Parent rootPane, OutputTab outputTab) {
         this.rootPane = rootPane;
@@ -40,143 +36,72 @@ public class InputTab {
     }
 
     void drawInputTab() {
-        var flowPane = ((FlowPane) rootPane.lookup("#inputFlowPane"));
-        var tableView = ((TableView) flowPane.getChildren().get(2));
         tableView.setItems(FXCollections.observableArrayList(LocalObjStorage.getTaskList().stream().filter(task -> task.getProject().getId() == JavaFXMain.selectedProjectId).collect(Collectors.toList())));
     }
 
     private void setupInputTab() {
         var flowPane = ((FlowPane) rootPane.lookup("#inputFlowPane"));
-        var tableView = ((TableView) flowPane.getChildren().get(2));
-        tableView.setItems(FXCollections.observableArrayList(LocalObjStorage.getTaskList().stream().filter(task -> task.getProject().getId() == JavaFXMain.selectedProjectId).collect(Collectors.toList())));
-        ((TableColumn) tableView.getColumns().get(0)).setCellValueFactory(new PropertyValueFactory<Task, String>("id"));
-        ((TableColumn) tableView.getColumns().get(1)).setCellValueFactory(new PropertyValueFactory<Task, String>("name"));
-        ((TableColumn) tableView.getColumns().get(2)).setCellValueFactory(new PropertyValueFactory<Task, String>("estimatedTime"));
-        ((TableColumn) tableView.getColumns().get(3)).setCellValueFactory(new PropertyValueFactory<Task, String>("priority"));
-        ((TableColumn) tableView.getColumns().get(4)).setCellValueFactory(new PropertyValueFactory<Task, String>("probabilities"));
-        ((TableColumn) tableView.getColumns().get(5)).setCellValueFactory(new PropertyValueFactory<Task, String>("dependencies"));
+
+        //Table view
+        tableView = ((TableView<Task>) flowPane.getChildren().get(2));
+        setUpTaskTable();
+
+        //Input view
         VBox inputVBox = ((VBox) flowPane.getChildren().get(0));
         TextField nameTextField = ((TextField) inputVBox.getChildren().get(1));
         TextField priority = ((TextField) inputVBox.getChildren().get(3));
-        priority.textProperty().addListener((observable, oldValue, newValue) -> {
-            if (!newValue.matches("\\d*")) {
-                priority.setText(newValue.replaceAll("[\\D]", ""));
-            }
-        });
         TextField estimatedTimeTextField = ((TextField) inputVBox.getChildren().get(5));
-        estimatedTimeTextField.textProperty().addListener((observable, oldValue, newValue) -> {
-            if (!newValue.matches("\\d*")) {
-                estimatedTimeTextField.setText(newValue.replaceAll("[\\D]", ""));
-            }
-        });
-        ListView<Task> listViewDeps = ((ListView<Task>) inputVBox.getChildren().get(11));
+        HBox probsHBox = ((HBox) inputVBox.getChildren().get(7));
+        HBox probsHBox2 = ((HBox) inputVBox.getChildren().get(8));
+        HBox probsHBox3 = ((HBox) inputVBox.getChildren().get(9));
+        ListView<Task> listViewDependency = ((ListView<Task>) inputVBox.getChildren().get(11));
+        HBox buttonsForDependencies = (HBox) inputVBox.getChildren().get(12);
+        HBox bottomButtonsHBox = (HBox) inputVBox.getChildren().get(13);
 
-        HBox prorbsHBox = ((HBox) inputVBox.getChildren().get(7));
-        TextField probs1 = ((TextField) prorbsHBox.getChildren().get(0));
-        probs1.textProperty().addListener((observable, oldValue, newValue) -> {
-            if (!newValue.matches("\\d*")) {
-                probs1.setText(newValue.replaceAll("[\\D]", ""));
-            }
-        });
-        TextField probs2 = ((TextField) prorbsHBox.getChildren().get(1));
-        probs2.textProperty().addListener((observable, oldValue, newValue) -> {
-            if (!newValue.matches("\\d*")) {
-                probs2.setText(newValue.replaceAll("[\\D]", ""));
-            }
-        });
-        HBox prorbsHBox2 = ((HBox) inputVBox.getChildren().get(8));
-        TextField probs3 = ((TextField) prorbsHBox2.getChildren().get(0));
-        probs3.textProperty().addListener((observable, oldValue, newValue) -> {
-            if (!newValue.matches("\\d*")) {
-                probs3.setText(newValue.replaceAll("[\\D]", ""));
-            }
-        });
-        TextField probs4 = ((TextField) prorbsHBox2.getChildren().get(1));
-        probs4.textProperty().addListener((observable, oldValue, newValue) -> {
-            if (!newValue.matches("\\d*")) {
-                probs4.setText(newValue.replaceAll("[\\D]", ""));
-            }
-        });
-        HBox prorbsHBox3 = ((HBox) inputVBox.getChildren().get(9));
-        TextField probs5 = ((TextField) prorbsHBox3.getChildren().get(0));
-        probs5.textProperty().addListener((observable, oldValue, newValue) -> {
-            if (!newValue.matches("\\d*")) {
-                probs5.setText(newValue.replaceAll("[\\D]", ""));
-            }
-        });
-        TextField probs6 = ((TextField) prorbsHBox3.getChildren().get(1));
-        probs6.textProperty().addListener((observable, oldValue, newValue) -> {
-            if (!newValue.matches("\\d*")) {
-                probs6.setText(newValue.replaceAll("[\\D]", ""));
-            }
-        });
+        priority.textProperty().addListener((observable, oldValue, newValue) -> validateNumericInput(priority, newValue));
+        estimatedTimeTextField.textProperty().addListener((observable, oldValue, newValue) -> validateNumericInput(estimatedTimeTextField, newValue));
 
-        HBox buttonsDeps = (HBox) inputVBox.getChildren().get(12);
-        buttonsDeps.getChildren().get(0).setOnMouseClicked(event -> {
-            int taskId = (int) ((TableColumn) tableView.getColumns().get(0)).getCellObservableValue(tableView.getSelectionModel().getSelectedIndex()).getValue();
-            Task task = LocalObjStorage.getTaskById(taskId);
-            if (!taskDependencies.contains(task)) {
+        TextField probs1 = ((TextField) probsHBox.getChildren().get(0));
+        probs1.textProperty().addListener((observable, oldValue, newValue) -> validateNumericInput(probs1, newValue));
+        TextField probs2 = ((TextField) probsHBox.getChildren().get(1));
+        probs2.textProperty().addListener((observable, oldValue, newValue) -> validateNumericInput(probs2, newValue));
 
-                taskDependencies.add(LocalObjStorage.getTaskById(taskId));
-                listViewDeps.setItems(FXCollections.observableArrayList(taskDependencies));
-            }
-        });
+        TextField probs3 = ((TextField) probsHBox2.getChildren().get(0));
+        probs3.textProperty().addListener((observable, oldValue, newValue) -> validateNumericInput(probs3, newValue));
+        TextField probs4 = ((TextField) probsHBox2.getChildren().get(1));
+        probs4.textProperty().addListener((observable, oldValue, newValue) -> validateNumericInput(probs4, newValue));
 
-        buttonsDeps.getChildren().get(1).setOnMouseClicked(event -> {
-            Task task = listViewDeps.getSelectionModel().getSelectedItem();
-            if (taskDependencies.contains(task)) {
-                taskDependencies.remove(task);
-                listViewDeps.setItems(FXCollections.observableArrayList(taskDependencies));
-            }
-        });
+        TextField probs5 = ((TextField) probsHBox3.getChildren().get(0));
+        probs5.textProperty().addListener((observable, oldValue, newValue) -> validateNumericInput(probs5, newValue));
+        TextField probs6 = ((TextField) probsHBox3.getChildren().get(1));
+        probs6.textProperty().addListener((observable, oldValue, newValue) -> validateNumericInput(probs6, newValue));
 
+        buttonsForDependencies.getChildren().get(0).setOnMouseClicked(event -> addDependency(listViewDependency));
+        buttonsForDependencies.getChildren().get(1).setOnMouseClicked(event -> removeDependency(listViewDependency));
 
-        HBox buttonHbox = (HBox) inputVBox.getChildren().get(13);
-        buttonHbox.getChildren().get(0).setOnMouseClicked(event -> {
-            Task ttt = new Task(nameTextField.getText(), Integer.parseInt(estimatedTimeTextField.getText()), Integer.parseInt(priority.getText()),
-                    LocalObjStorage.getProjectById(JavaFXMain.selectedProjectId));
+        bottomButtonsHBox.getChildren().get(0).setOnMouseClicked(event -> {
+            List<Probabilities> probabilities = new ArrayList<>();
             if (!probs1.getText().equals(""))
-                ttt.getProbabilities().add(new Probabilities(Integer.parseInt(probs1.getText()), Integer.parseInt(probs2.getText())));
+                probabilities.add(new Probabilities(Double.parseDouble(probs1.getText()), Double.parseDouble(probs2.getText())));
             if (!probs3.getText().equals(""))
-                ttt.getProbabilities().add(new Probabilities(Integer.parseInt(probs3.getText()), Integer.parseInt(probs4.getText())));
+                probabilities.add(new Probabilities(Double.parseDouble(probs3.getText()), Double.parseDouble(probs4.getText())));
             if (!probs5.getText().equals(""))
-                ttt.getProbabilities().add(new Probabilities(Integer.parseInt(probs5.getText()), Integer.parseInt(probs6.getText())));
-            ttt.addDependency(taskDependencies);
-            tableView.setItems(FXCollections.observableArrayList(LocalObjStorage.getTaskList().
-                    stream().filter(task -> task.getProject().getId() == JavaFXMain.selectedProjectId).collect(Collectors.toList())));
+                probabilities.add(new Probabilities(Double.parseDouble(probs5.getText()), Double.parseDouble(probs6.getText())));
+            addTask(nameTextField.getText(), Double.parseDouble(estimatedTimeTextField.getText()),
+                    Integer.parseInt(priority.getText()), probabilities, tableView);
         });
-        System.out.println(buttonHbox.getChildren().get(1).getClass().getSimpleName());
-        buttonHbox.getChildren().get(1).setOnMouseClicked(event -> {
-            probs1.clear();
-            probs2.clear();
-            probs3.clear();
-            probs4.clear();
-            probs5.clear();
-            probs6.clear();
-            priority.clear();
-            nameTextField.clear();
-            estimatedTimeTextField.clear();
-            taskDependencies.clear();
-            listViewDeps.setItems(FXCollections.observableArrayList(taskDependencies));
-        });
-        VBox vboxSplitter = ((VBox) ((Pane) flowPane.getChildren().get(1)).getChildren().get(0));
-        vboxSplitter.getChildren().get(1).setOnMouseClicked(event -> calc(LocalObjStorage.getProjectById(JavaFXMain.selectedProjectId), ((CheckBox) vboxSplitter.getChildren().get(2)).isSelected()));
-        vboxSplitter.getChildren().get(0).setOnMouseClicked(event -> {
-            int taskId = (int) ((TableColumn) tableView.getColumns().get(0)).getCellObservableValue(tableView.getSelectionModel().getSelectedIndex()).getValue();
+        bottomButtonsHBox.getChildren().get(1).setOnMouseClicked(event -> clearInputFields(listViewDependency, probs1, probs2, probs3,
+                probs4, probs5, probs6, nameTextField, estimatedTimeTextField, priority));
 
-            Project project = LocalObjStorage.getProjectById(JavaFXMain.selectedProjectId);
-            project.getTasks().remove(LocalObjStorage.getTaskById(taskId));
-            project.setSequence("");
-            DatabaseManager.removeTask(taskId);
-            LocalObjStorage.getTaskList().remove(LocalObjStorage.getTaskById(taskId));
 
-            tableView.setItems(FXCollections.observableArrayList(LocalObjStorage.getTaskList().
-                    stream().filter(task -> task.getProject().getId() == JavaFXMain.selectedProjectId).collect(Collectors.toList())));
-        });
+        //Middle column
+        VBox vBoxSplitter = ((VBox) ((Pane) flowPane.getChildren().get(1)).getChildren().get(0));
+        vBoxSplitter.getChildren().get(1).setOnMouseClicked(event -> calculate(LocalObjStorage.getProjectById(JavaFXMain.selectedProjectId), ((CheckBox) vBoxSplitter.getChildren().get(2)).isSelected()));
+        vBoxSplitter.getChildren().get(0).setOnMouseClicked(event -> removeTask());
         drawInputTab();
     }
 
-    private void calc(Project pro, boolean useMonty) {
+    private void calculate(Project pro, boolean useMonty) {
         Instant start = java.time.Instant.now();
 
         Sequence.sequenceTasks(pro, useMonty);
@@ -189,4 +114,64 @@ public class InputTab {
                 between.toHours(), between.toMinutes(), between.getSeconds(), between.toMillis()); // 0D, 00:00:01.1001
     }
 
+    private void validateNumericInput(TextField textField, String newValue) {
+        if (!newValue.matches("\\d*")) {
+            textField.setText(newValue.replaceAll("[\\D]", ""));
+        }
+    }
+
+    private void clearInputFields(ListView<Task> listViewDependency, TextField... textFields) {
+        for (TextField textField : textFields)
+            textField.clear();
+        taskDependencies.clear();
+        listViewDependency.setItems(FXCollections.observableArrayList(taskDependencies));
+    }
+
+    private void addTask(String name, double estimatedTime, int priority, List<Probabilities> probabilities, TableView<Task> tableView) {
+        Task ttt = new Task(name, estimatedTime, priority,
+                LocalObjStorage.getProjectById(JavaFXMain.selectedProjectId));
+        for (Probabilities pro : probabilities)
+            ttt.getProbabilities().add(pro);
+        ttt.addDependency(taskDependencies);
+        tableView.setItems(FXCollections.observableArrayList(LocalObjStorage.getTaskList().
+                stream().filter(task -> task.getProject().getId() == JavaFXMain.selectedProjectId).collect(Collectors.toList())));
+    }
+
+    private void removeDependency(ListView<Task> listViewDependency) {
+        Task task = listViewDependency.getSelectionModel().getSelectedItem();
+        if (taskDependencies.contains(task)) {
+            taskDependencies.remove(task);
+            listViewDependency.setItems(FXCollections.observableArrayList(taskDependencies));
+        }
+    }
+
+    private void addDependency(ListView<Task> listViewDependency) {
+        int taskId = (int) ((TableColumn) tableView.getColumns().get(0)).getCellObservableValue(tableView.getSelectionModel().getSelectedIndex()).getValue();
+        Task task = LocalObjStorage.getTaskById(taskId);
+        if (!taskDependencies.contains(task)) {
+            taskDependencies.add(LocalObjStorage.getTaskById(taskId));
+            listViewDependency.setItems(FXCollections.observableArrayList(taskDependencies));
+        }
+    }
+
+    private void removeTask() {
+        int taskId = (int) ((TableColumn) tableView.getColumns().get(0)).getCellObservableValue(tableView.getSelectionModel().getSelectedIndex()).getValue();
+        Project project = LocalObjStorage.getProjectById(JavaFXMain.selectedProjectId);
+        project.getTasks().remove(LocalObjStorage.getTaskById(taskId));
+        project.setSequence("");
+        DatabaseManager.removeTask(taskId);
+        LocalObjStorage.getTaskList().remove(LocalObjStorage.getTaskById(taskId));
+        tableView.setItems(FXCollections.observableArrayList(LocalObjStorage.getTaskList().
+                stream().filter(task -> task.getProject().getId() == JavaFXMain.selectedProjectId).collect(Collectors.toList())));
+    }
+
+    private void setUpTaskTable() {
+        tableView.setItems(FXCollections.observableArrayList(LocalObjStorage.getTaskList().stream().filter(task -> task.getProject().getId() == JavaFXMain.selectedProjectId).collect(Collectors.toList())));
+        tableView.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
+        tableView.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("name"));
+        tableView.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("estimatedTime"));
+        tableView.getColumns().get(3).setCellValueFactory(new PropertyValueFactory<>("priority"));
+        tableView.getColumns().get(4).setCellValueFactory(new PropertyValueFactory<>("probabilities"));
+        tableView.getColumns().get(5).setCellValueFactory(new PropertyValueFactory<>("dependencies"));
+    }
 }
