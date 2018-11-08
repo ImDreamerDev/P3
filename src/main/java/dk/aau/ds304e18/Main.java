1cd5a6013e2b9a1b48787388b8caf0599cb5a55a
Milton <mlindo17@student.aau.dk>
2018-11-08 16:41:36 +0100

    rasmus fix

--- src/main/java/dk/aau/ds304e18/Main.java
+++ src/main/java/dk/aau/ds304e18/Main.java
@@ -9,7 +9,7 @@ import dk.aau.ds304e18.models.Task;
 
 class Main {
     public static void main(String[] args) {
-        DatabaseManager.distributeModels();
+        //DatabaseManager.distributeModels();
         ProjectManager projectManager = new ProjectManager("Milton", "hardcore");
         Employee rasmus = new Employee("Rasmus Smit Lindholt");
         Project testProj = new Project("Dank communication things to communicate with things", projectManager);
