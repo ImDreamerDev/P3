fc20abd4f8e5a3ea9f60997cba03225a393926b1
Milton <mlindo17@student.aau.dk>
2018-11-09 12:51:41 +0100

    Stuff fix maybe

---
+++ src/main/java/dk/aau/ds304e18/math/Calc.java
@@ -0,0 +1,15 @@
+package dk.aau.ds304e18.math;
+
+public class Calc {
+
+    //TODO: Optimize this, right now it's O(n), it can be done better I think
+    public static int amountMax(int amount) {
+
+        if(amount != 1)
+            return amount*amountMax(amount-1);
+
+        return 1;
+
+    }
+
+}
