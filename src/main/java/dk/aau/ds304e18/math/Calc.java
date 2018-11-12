896e6542d366de8701071d428ff7a0d91ee0c4f2
Milton <mlindo17@student.aau.dk>
2018-11-12 15:19:21 +0100

    Stuff doesn't overflow anymore, but god help your computer if you hit
    that large number (Which we will almost every time when there are so many tasks fuck I'm optimizing ree ok bai

--- src/main/java/dk/aau/ds304e18/math/Calc.java
+++ src/main/java/dk/aau/ds304e18/math/Calc.java
@@ -4,6 +4,8 @@ public class Calc {
 
     //TODO: Optimize this, right now it's O(n), it can be done better I think
     public static int amountMax(int amount) {
+        if (amount > 12)
+            return Integer.MAX_VALUE;
         if (amount != 1)
             return amount * amountMax(amount - 1);
         return 1;
