378f5e72dc8234132408eb88df0973a69dc4d0b8
Lasse Rasmussen <freedomforce360@gmail.com>
2018-11-16 10:48:51 +0100

    Closes the pop when add is pressed

--- src/main/java/dk/aau/ds304e18/ui/InputTab.java
+++ src/main/java/dk/aau/ds304e18/ui/InputTab.java
@@ -344,6 +344,7 @@ public class InputTab {
                 listViewDependency.setItems(FXCollections.observableArrayList(taskDependencies));
             }
         }
+        closeDependenciesPopup();
     }
 
     /**
