6cf2f3257f66fc12962965f953fe7ca8ac109d53
Lasse Rasmussen <freedomforce360@gmail.com>
2018-11-20 15:40:45 +0100

    All most all the GUI files commented

--- src/main/java/dk/aau/ds304e18/gui/Login.java
+++ src/main/java/dk/aau/ds304e18/gui/Login.java
@@ -16,6 +16,9 @@ import javafx.scene.layout.*;
 
 import java.util.concurrent.atomic.AtomicReference;
 
+/**
+ * The login pane.
+ */
 public class Login {
     /**
      * The root pane of the GUI.
@@ -37,36 +40,57 @@ public class Login {
      */
     private final VBox vBoxLogin;
 
+    /**
+     * Creates and sets up the login
+     *
+     * @param image    The background for the login screen.
+     * @param rootPane The root pane of the GUI.
+     */
     public Login(Image image, Parent rootPane) {
         this.rootPane = rootPane;
+        //Create the background.
         BackgroundImage backgroundImage = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT,
                 new BackgroundPosition(Side.LEFT, 0.5, true, Side.TOP, 0.1,
                         true), new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO,
                 false, false, false, false));
 
-        ((Pane) this.rootPane.lookup("#loginPane")).setBackground(new Background(backgroundImage));
-        this.rootPane.lookup("#loginPane").getParent().setVisible(true);
-        vBoxLogin = ((VBox) this.rootPane.lookup("#loginPane"));
+        //Sets the background of the login screen.
+        ((Pane) rootPane.lookup("#loginPane")).setBackground(new Background(backgroundImage));
+
+        //Set the login screen visible.
+        rootPane.lookup("#loginPane").getParent().setVisible(true);
+
+        //Sets the login pane.
+        vBoxLogin = ((VBox) rootPane.lookup("#loginPane"));
+
+        //Gets the login box from the login pane.
         Button loginButton = ((Button) vBoxLogin.getChildrenUnmodifiable().get(3));
+        //Gets the username from the login pane.
         usernameField = (TextField) ((HBox) vBoxLogin.getChildrenUnmodifiable().get(0)).getChildren().get(1);
 
+        //If enter is pressed when username field is focused login.
         usernameField.setOnKeyPressed(ke -> {
             if (ke.getCode().equals(KeyCode.ENTER)) {
                 logIn();
             }
         });
 
+        //Gets the password for the login pane.
         passwordField = (PasswordField) ((HBox) vBoxLogin.getChildrenUnmodifiable().get(1)).getChildren().get(1);
 
+        //If enter is pressed when password field is focused login.
         passwordField.setOnKeyPressed(ke -> {
             if (ke.getCode().equals(KeyCode.ENTER)) {
                 logIn();
             }
         });
+        //Sets the login button to login when pressed.
         loginButton.setOnMouseClicked(event -> logIn());
-
     }
 
+    /**
+     * Focus the username.
+     */
     public void focusUsername() {
         usernameField.requestFocus();
     }
@@ -78,7 +102,7 @@ public class Login {
         //Get the username and password from their fields.
         String username = usernameField.getText();
         String password = passwordField.getText();
-        
+
         //Try to login with the username and password.
         AtomicReference<ProjectManager> pm = new AtomicReference<>(DatabaseManager.logIn(username, password));
 
@@ -87,27 +111,38 @@ public class Login {
             Label error = ((Label) vBoxLogin.getChildren().get(2));
             error.setText("Error: Username and Password does not match any user");
             error.setVisible(true);
+            //If the pms name = Connection error the program couldn't reach the database.    
         } else if (pm.get().getName().equals("Connection error")) {
             Label error = ((Label) vBoxLogin.getChildren().get(2));
             error.setText("Error: Couldn't connect to the database");
             error.setVisible(true);
+            //Otherwise the login was successful.    
         } else {
             Label error = ((Label) vBoxLogin.getChildren().get(2));
             error.setText("");
             error.setVisible(false);
             Task<Void> voidTask = DatabaseManager.distributeModels(pm.get());
+            //Create and bind a progressbar to show the process on downloading and setting up everything.
             ProgressBar bar = new ProgressBar();
             bar.progressProperty().bind(voidTask.progressProperty());
             ((StackPane) rootPane.getChildrenUnmodifiable().get(2)).getChildren().add(bar);
+
+            //When the task succeeds
             voidTask.setOnSucceeded(observable -> {
+                //Remove the progressbar.
                 ((StackPane) rootPane.getChildrenUnmodifiable().get(2)).getChildren().remove(bar);
+                //Update the project manager, with the new information from the database.
                 pm.set(LocalObjStorage.getProjectManagerById(pm.get().getId()));
+                //Create all the tabs.
                 JavaFXMain.outputTab = new OutputTab(rootPane);
                 JavaFXMain.inputTab = new InputTab(rootPane);
                 JavaFXMain.projectTab = new ProjectTab(rootPane, pm.get());
+                //Hide the login pane.
                 rootPane.getChildrenUnmodifiable().get(2).setVisible(false);
             });
+            //When the task fails set the progressbar red.
             voidTask.setOnFailed(observable -> bar.setStyle("-fx-progress-color: red"));
+            //Start the task.
             new Thread(voidTask).start();
         }
     }
--- src/main/java/dk/aau/ds304e18/gui/ProjectTab.java
+++ src/main/java/dk/aau/ds304e18/gui/ProjectTab.java
@@ -17,65 +17,118 @@ import javafx.scene.layout.HBox;
 
 import java.util.stream.Collectors;
 
+/**
+ * The main tab of the program.
+ * Displaying all the projects owned by the user.
+ */
 public class ProjectTab {
 
+    /**
+     * The root pane of the GUI.
+     */
     private final Parent rootPane;
+    /**
+     * The sorted list of projects.
+     */
     private SortedList<Project> sortedList;
+
+    /**
+     * The logged in project manager.
+     */
     private final ProjectManager projectManager;
+
+    /**
+     * The project table view.
+     */
     private TableView<Project> tableView;
+
+    /**
+     * The filter list of projects.
+     */
     private FilteredList<Project> flProjects;
+
+    /**
+     * The project toolbar.
+     */
     private HBox projectToolBar;
+
+    /**
+     * The main tab pane.
+     */
     private final TabPane tabPane;
 
 
     public ProjectTab(Parent rootPane, ProjectManager projectManager) {
+        //Init the values.
         this.projectManager = projectManager;
         this.rootPane = rootPane;
-        setupProjectTab();
         tabPane = ((TabPane) rootPane.getChildrenUnmodifiable().get(1));
+
+        //Setup the project tab.
+        setupProjectTab();
     }
 
+    /**
+     * Update the project list
+     *
+     * @return Returns a sorted list of all the projects in the local object storage.
+     */
     private SortedList<Project> updateProjects() {
         FilteredList<Project> flProjects = new FilteredList<>
                 (FXCollections.observableArrayList(LocalObjStorage.getProjectList()));
         return new SortedList<>(flProjects);
     }
 
-    @SuppressWarnings("unchecked")
-    private void setupProjectTab() {
 
+    private void setupProjectTab() {
+        //Get the table view from the GUI.
         tableView = ((TableView<Project>) rootPane.lookup("#projectView"));
+
+        //Get the left project toolbar from the GUI.
         projectToolBar = ((HBox) rootPane.lookup("#projectToolbar"));
+        //Set up the project tab.
         setUpProjectTable();
+        //If a table element is click more than once, open the input tab for that project.
         tableView.setOnMouseClicked(event -> {
             onTableElementSelected();
-            if (event.getClickCount() == 2) {
+            if (event.getClickCount() > 1) {
                 if (JavaFXMain.selectedProjectId != 0)
                     tabPane.getSelectionModel().select(tabPane.getTabs().get(1));
             }
         });
+        //Sort the table view after id.
         tableView.getSortOrder().add(tableView.getColumns().get(0));
 
+        //Get the New project button from the GUI.
         Button createButton = ((Button) projectToolBar.getChildren().get(2));
+        //Set the tool tip of the new project button.
         createButton.setTooltip(new Tooltip("Creates a new project with the selected name"));
+
+        //Create a new project when the button is clicked.
         createButton.setOnMouseClicked(event ->
                 createProject(((TextField) projectToolBar.getChildrenUnmodifiable().get(1))));
 
-        HBox projectToolBar2 = ((HBox) rootPane.lookup("#projectToolbar2"));
+        //Get the project Toolbar to the right from the GUI.
+        HBox projectToolBarRight = ((HBox) rootPane.lookup("#projectToolbar2"));
 
-        Button archiveButton = ((Button) projectToolBar2.getChildren().get(0));
+        //Gets the archived button from the GUI.
+        Button archiveButton = ((Button) projectToolBarRight.getChildren().get(0));
+        //Sets the tool tip for the archive button.
         archiveButton.setTooltip(new Tooltip("Archives the selected project"));
+        //Sets the archive button to call the archive project method when pressed.
         archiveButton.setOnMouseClicked(event -> archiveProject());
 
-        TextField searchField = ((TextField) projectToolBar2.getChildren().get(2));
+        //Gets the search field from the GUI.
+        TextField searchField = ((TextField) projectToolBarRight.getChildren().get(2));
+        //Sets the prompt text for the search field.
         searchField.setPromptText("Search here!");
+        //When something is typed inside of the search field call search.
         searchField.setOnKeyReleased(keyEvent -> search(searchField));
 
-        CheckBox showArchived = ((CheckBox) projectToolBar2.getChildren().get(3));
+        //Gets the show archived checkbox from the GUI.
+        CheckBox showArchived = ((CheckBox) projectToolBarRight.getChildren().get(3));
         showArchived.selectedProperty().addListener((ov, old_val, new_val) -> onShowArchived(new_val));
         showArchived.setSelected(false);
-
-
     }
 
     /**
@@ -87,6 +140,7 @@ public class ProjectTab {
      * @return true - if it is a letter - false if is a number.
      */
     private boolean isFirstLetter(String str) {
+        //If the str is parsable it's a int otherwise it's a character.
         try {
             Integer.parseInt(str);
         } catch (NumberFormatException nfe) {
@@ -101,16 +155,21 @@ public class ProjectTab {
      * @param projectName - the name of the project.
      */
     private void createProject(TextField projectName) {
+        //If we try to create a project and the name is blank make the border red and return.
         if (projectName.getText().isBlank()) {
             projectName.setStyle("-fx-border-color: red");
             return;
         }
+        //Remove the red border.
         projectName.setStyle("");
+        //Create the new project with the name and the logged in user.
         new Project(projectName.getText().trim(), projectManager);
+        //Clear the project name field.
         projectName.clear();
         sortedList = updateProjects();
+        //Bind the list to the table.
         sortedList.comparatorProperty().bind(tableView.comparatorProperty());
-
+        //Clears the items to make sure the correct data is displayed.
         tableView.getItems().clear();
         tableView.setItems(FXCollections.observableArrayList(sortedList.stream().filter(project -> project.getState() ==
                 ProjectState.ONGOING && project.getCreator() != null && project.getCreator()
@@ -123,11 +182,13 @@ public class ProjectTab {
      * @param searchField - the input field for the text.
      */
     private void search(TextField searchField) {
+        //If the fist character is a letter, we are searching by name. 
+        //Otherwise we are searching by id.
         if (isFirstLetter(searchField.getText())) {
             flProjects.setPredicate(p -> Integer.toString(p.getId()).contains(searchField.getText().toLowerCase().trim()));
         } else
             flProjects.setPredicate(p -> p.getName().toLowerCase().contains(searchField.getText().toLowerCase().trim()));
-
+        //Set the appropriate items.
         tableView.setItems(flProjects);
     }
 
@@ -135,31 +196,31 @@ public class ProjectTab {
      * Method for setting up the table on the project tab.
      */
     private void setUpProjectTable() {
+        //Set the columns of the table view to the display the fields named in "".
         tableView.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
         tableView.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("name"));
         tableView.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("creator"));
         tableView.getColumns().get(3).setCellValueFactory(new PropertyValueFactory<>("sequence"));
         tableView.getColumns().get(4).setCellValueFactory(new PropertyValueFactory<>("duration"));
         tableView.getColumns().get(5).setCellValueFactory(new PropertyValueFactory<>("state"));
+
+        //Get the 7th column and set the data equal to the number of tasks in the project.
         TableColumn<Project, String> column = (TableColumn<Project, String>) tableView.getColumns().get(6);
         column.setCellValueFactory(cellData ->
                 new SimpleStringProperty(cellData.getValue().getTasks().size() + ""));
 
+        //Set filtered list to all the projects in local storage.
         flProjects = (new FilteredList<>(FXCollections.observableArrayList(LocalObjStorage.getProjectList())));
+        //Create a sorted list to make the list both sortable and searchable.
         sortedList = new SortedList<>(flProjects);
-        sortedList.comparatorProperty().
+        //Bind the list to the table.
+        sortedList.comparatorProperty().bind(tableView.comparatorProperty());
 
-                bind(tableView.comparatorProperty());
+        //Sets the tables items equal to all the projects owned by the user and is ongoing.
         tableView.setItems(FXCollections.observableArrayList(sortedList.stream().
-
                 filter(project -> project.getState() ==
                         ProjectState.ONGOING && project.getCreator() != null && project.getCreator()
-                        .
-
-                                getId() == projectManager.getId()).
-
-                collect(Collectors.toList())));
-
+                        .getId() == projectManager.getId()).collect(Collectors.toList())));
     }
 
     /**
@@ -168,6 +229,8 @@ public class ProjectTab {
      * @param new_val The new value of the button
      */
     private void onShowArchived(boolean new_val) {
+        //If the new value is false draw only current projects. Otherwise draw all projects assigned to the 
+        // project manager.
         if (!new_val)
             tableView.setItems(FXCollections.observableArrayList(sortedList.stream().filter(project -> project.getState()
                     == ProjectState.ONGOING && project.getCreator() != null && project
@@ -177,33 +240,48 @@ public class ProjectTab {
                     .filter(project -> project.getCreator() != null && project.getCreator().getId()
                             == projectManager.getId()).collect(Collectors.toList())));
 
+        //Sort the table after id.
         tableView.getSortOrder().add(tableView.getColumns().get(0));
     }
 
     /**
-     * Method for selecting a project.
+     * Called when a element in the table view is selected.
      */
     private void onTableElementSelected() {
-        if (tableView.getSelectionModel().getSelectedIndex() != -1 && JavaFXMain.selectedProjectId !=
-                ((int) ((TableColumn) tableView.getColumns().get(0)).getCellObservableValue(
-                        tableView.getSelectionModel().getSelectedIndex()).getValue())) {
-            JavaFXMain.selectedProjectId = ((int) ((TableColumn) tableView.getColumns().get(0))
-                    .getCellObservableValue(tableView.getSelectionModel().getSelectedIndex()).getValue());
-            JavaFXMain.inputTab.drawInputTab();
-            ((Label) projectToolBar.getChildren().get(4)).setText("Selected: " +
-                    LocalObjStorage.getProjectById(JavaFXMain.selectedProjectId).getName());
-            TabPane tabPane = (TabPane) rootPane.getChildrenUnmodifiable().get(1);
-            tabPane.getTabs().get(1).setDisable(false);
-            tabPane.getTabs().get(2).setDisable(false);
+        //If nothing is selected return.
+        if (tableView.getSelectionModel().getSelectedIndex() == -1 || JavaFXMain.selectedProjectId ==
+                ((int) ((TableColumn) tableView.getColumns().get(0)).getCellObservableValue(tableView.getSelectionModel()
+                        .getSelectedIndex()).getValue())) {
+            return;
         }
+        //Update the selected project id.
+        JavaFXMain.selectedProjectId = ((int) ((TableColumn) tableView.getColumns().get(0))
+                .getCellObservableValue(tableView.getSelectionModel().getSelectedIndex()).getValue());
+
+        //Update the input tab.
+        JavaFXMain.inputTab.drawInputTab();
+
+        //Set the selected label equal to the selected project name.
+        ((Label) projectToolBar.getChildren().get(4)).setText("Selected: " +
+                LocalObjStorage.getProjectById(JavaFXMain.selectedProjectId).getName());
+
+        //Get the tabs from the GUI.
+        TabPane tabPane = (TabPane) rootPane.getChildrenUnmodifiable().get(1);
+        //Enable the other tabs when project is selected.
+        tabPane.getTabs().get(1).setDisable(false);
+        tabPane.getTabs().get(2).setDisable(false);
     }
 
     /**
      * Method for archiving an on going project.
      */
     private void archiveProject() {
+        //If a project is selected.
         if (JavaFXMain.selectedProjectId != 0) {
+            //Set the current project as a old project.
             projectManager.addOldProject(LocalObjStorage.getProjectById(JavaFXMain.selectedProjectId));
+
+            //Update the table view depending on if the show archived is true.
             if (!((CheckBox) rootPane.lookup("#showArchivedCheckbox")).isSelected())
                 tableView.setItems(FXCollections.observableArrayList(sortedList.stream().filter(project -> project.getState()
                         == ProjectState.ONGOING && project.getCreator() != null && project.getCreator()
