c777548fba2f5d1e2359f94450f37fc6a8339c10
Rasmus Smit <rasmus-sl@hotmail.com>
2018-11-20 11:22:49 +0100

    A bit of refactor so it's not entirely copy-pasta

--- src/main/java/dk/aau/ds304e18/database/Password.java
+++ src/main/java/dk/aau/ds304e18/database/Password.java
@@ -13,9 +13,6 @@ public class Password {
     private static final int ITERATIONS = 10000;
     private static final int KEY_LENGTH = 256;
 
-    /**
-     * static utility class
-     */
     private Password() {
     }
 
@@ -31,29 +28,29 @@ public class Password {
     }
 
     /**
-     * Returns a salted and hashed password using the provided hash.<br>
-     * Note - side effect: the password is destroyed (the char[] is filled with zeros)
+     * Returns a salted and hashed password using the provided hash.
+     * Note - side effect: the char[] is filled with zeros
      *
      * @param password the password to be hashed
-     * @param salt     a 16 bytes salt, ideally obtained with the getNextSalt method
+     * @param salt     a 16 bytes salt, obtained with the getNextSalt method
      * @return the hashed password with a pinch of salt
      */
     public static byte[] hash(char[] password, byte[] salt) {
-        PBEKeySpec spec = new PBEKeySpec(password, salt, ITERATIONS, KEY_LENGTH);
+        PBEKeySpec keySpec = new PBEKeySpec(password, salt, ITERATIONS, KEY_LENGTH);
         Arrays.fill(password, Character.MIN_VALUE);
         try {
-            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-            return skf.generateSecret(spec).getEncoded();
+            SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            return secretKeyFactory.generateSecret(keySpec).getEncoded();
         } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
             throw new AssertionError("Error while hashing a password: " + e.getMessage(), e);
         } finally {
-            spec.clearPassword();
+            keySpec.clearPassword();
         }
     }
 
     /**
-     * Returns true if the given password and salt match the hashed value, false otherwise.<br>
-     * Note - side effect: the password is destroyed (the char[] is filled with zeros)
+     * Returns true if the given password and salt match the hashed value, false otherwise.
+     * Note - side effect: the the char[] is filled with zeros
      *
      * @param password     the password to check
      * @param salt         the salt used to hash the password
@@ -61,11 +58,11 @@ public class Password {
      * @return true if the given password and salt match the hashed value, false otherwise
      */
     public static boolean isExpectedPassword(char[] password, byte[] salt, byte[] expectedHash) {
-        byte[] pwdHash = hash(password, salt);
+        byte[] passHash = hash(password, salt);
         Arrays.fill(password, Character.MIN_VALUE);
-        if (pwdHash.length != expectedHash.length) return false;
-        for (int i = 0; i < pwdHash.length; i++) {
-            if (pwdHash[i] != expectedHash[i]) return false;
+        if (passHash.length != expectedHash.length) return false;
+        for (int i = 0; i < passHash.length; i++) {
+            if (passHash[i] != expectedHash[i]) return false;
         }
         return true;
     }
