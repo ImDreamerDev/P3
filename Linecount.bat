093c15bdbb33608c189581800e9dfdd70a206361
Lasse Rasmussen <freedomforce360@gmail.com>
2018-11-21 14:41:14 +0100

    Show different stats

--- Linecount.bat
+++ Linecount.bat
@@ -1,6 +1,14 @@
 @echo off
 cls
-echo ------------------------------P3-----------------------------------------
-LineCounter\cloc "src"   
+echo ------------------------------P3-Code----------------------------------------
+LineCounter\cloc "src/main"
+pause
+cls
+echo ------------------------------P3-Test-----------------------------------
+LineCounter\cloc "src/test"
 echo Press enter to see total
+pause
+cls
+echo ------------------------------P3-Total---------------------------------------
+LineCounter\cloc "src"
 pause
