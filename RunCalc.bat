01677a5b4aa5b6c3391fd1039c2fff650bc39c17
Lasse Rasmussen <freedomforce360@gmail.com>
2018-11-22 15:28:18 +0100

    Update run config

--- RunCalc.bat
+++ RunCalc.bat
@@ -1,1 +1,6 @@
+cls
 java -jar --module-path %PATH_TO_FX% --add-modules=javafx.controls --add-modules=javafx.fxml target/P3-1.0-SNAPSHOT-jar-with-dependencies.jar --username=ct --password=1 --projectId=1 --numOfEmps=2
+
+java -jar --module-path %PATH_TO_FX% --add-modules=javafx.controls --add-modules=javafx.fxml target/P3-1.0-SNAPSHOT-jar-with-dependencies.jar --username=ct --password=1 --projectId=1 --numOfEmps=10
+
+java -jar --module-path %PATH_TO_FX% --add-modules=javafx.controls --add-modules=javafx.fxml target/P3-1.0-SNAPSHOT-jar-with-dependencies.jar --username=ct --password=1 --projectId=1 --numOfEmps=15
