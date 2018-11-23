80edd1b1f91cd0086ba9f70e4baccb60a334cf6d
Lasse Rasmussen <freedomforce360@gmail.com>
2018-11-23 15:38:50 +0100

    Better folders

--- RunCalc.bat
+++ RunCalc.bat
@@ -4,14 +4,23 @@ set day=%date:~,2%
 set month=%date:~3,2%
 set hour=%time:~,2%
 set min=%time:~3,2%
-mkdir "tests\%computername%"
+mkdir "tests\%computername%\p1\emp2"
+mkdir "tests\%computername%\p1\emp10"
 
-java -jar --module-path "%PATH_TO_FX%" --add-modules=javafx.controls --add-modules=javafx.fxml target/P3-1.0-SNAPSHOT-jar-with-dependencies.jar --username=ct --password=1 --projectId=1 --numOfEmps=2  > "tests\%computername%\p1-emp2-D-%day%-%month%-T-%hour%-%min%.txt"
+mkdir "tests\%computername%\p2\emp2"
+mkdir "tests\%computername%\p2\emp10"
+mkdir "tests\%computername%\p2\emp15"
 
-java -jar --module-path "%PATH_TO_FX%" --add-modules=javafx.controls --add-modules=javafx.fxml target/P3-1.0-SNAPSHOT-jar-with-dependencies.jar --username=ct --password=1 --projectId=1 --numOfEmps=10 > "tests\%computername%\p1-emp10-D-%day%-%month%-T-%hour%-%min%.txt"
+mkdir "tests\%computername%\p3\emp2"
+mkdir "tests\%computername%\p3\emp10"
+mkdir "tests\%computername%\p3\emp15"
 
-java -jar --module-path "%PATH_TO_FX%" --add-modules=javafx.controls --add-modules=javafx.fxml target/P3-1.0-SNAPSHOT-jar-with-dependencies.jar --username=ct --password=1 --projectId=2 --numOfEmps=2  > "tests\%computername%\p2-emp2-D-%day%-%month%-T-%hour%-%min%.txt"
+java -jar --module-path "%PATH_TO_FX%" --add-modules=javafx.controls --add-modules=javafx.fxml target/P3-1.0-SNAPSHOT-jar-with-dependencies.jar --username=ct --password=1 --projectId=1 --numOfEmps=2  > "tests\%computername%\p1\emp2\%day%-%month%-T-%hour%-%min%.txt"
 
-java -jar --module-path "%PATH_TO_FX%" --add-modules=javafx.controls --add-modules=javafx.fxml target/P3-1.0-SNAPSHOT-jar-with-dependencies.jar --username=ct --password=1 --projectId=2 --numOfEmps=10 > "tests\%computername%\p2-emp10-D-%day%-%month%-T-%hour%-%min%.txt"
+java -jar --module-path "%PATH_TO_FX%" --add-modules=javafx.controls --add-modules=javafx.fxml target/P3-1.0-SNAPSHOT-jar-with-dependencies.jar --username=ct --password=1 --projectId=1 --numOfEmps=10 > "tests\%computername%\p1\emp10\%day%-%month%-T-%hour%-%min%.txt"
 
-java -jar --module-path "%PATH_TO_FX%" --add-modules=javafx.controls --add-modules=javafx.fxml target/P3-1.0-SNAPSHOT-jar-with-dependencies.jar --username=ct --password=1 --projectId=2 --numOfEmps=20 > "tests\%computername%\p2-emp20-D-%day%-%month%-T-%hour%-%min%.txt"
+java -jar --module-path "%PATH_TO_FX%" --add-modules=javafx.controls --add-modules=javafx.fxml target/P3-1.0-SNAPSHOT-jar-with-dependencies.jar --username=ct --password=1 --projectId=2 --numOfEmps=2  > "tests\%computername%\p2\emp2\%day%-%month%-T-%hour%-%min%.txt"
+
+java -jar --module-path "%PATH_TO_FX%" --add-modules=javafx.controls --add-modules=javafx.fxml target/P3-1.0-SNAPSHOT-jar-with-dependencies.jar --username=ct --password=1 --projectId=2 --numOfEmps=10 > "tests\%computername%\p2\emp10\%day%-%month%-T-%hour%-%min%.txt"
+
+java -jar --module-path "%PATH_TO_FX%" --add-modules=javafx.controls --add-modules=javafx.fxml target/P3-1.0-SNAPSHOT-jar-with-dependencies.jar --username=ct --password=1 --projectId=2 --numOfEmps=20 > "tests\%computername%\p2\emp15\%day%-%month%-T-%hour%-%min%.txt"
