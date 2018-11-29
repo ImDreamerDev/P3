9f8a8ea8fa36d7503b8698d3419dc05caf98302c
Milton <mlindo17@student.aau.dk>
2018-11-29 16:06:30 +0100

    MINE 800 EZ

--- src/test/java/dk/aau/ds304e18/gui/input/EmployeeTabTest.java
+++ src/test/java/dk/aau/ds304e18/gui/input/EmployeeTabTest.java
@@ -1,4 +1,45 @@
 package dk.aau.ds304e18.gui.input;
 
+import dk.aau.ds304e18.database.DatabaseManager;
+import dk.aau.ds304e18.models.Task;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class EmployeeTabTest {
+    private Parent rootPane;
+    private static ListView listView;
+
+    @BeforeAll
+    static void realInit() {
+        DatabaseManager.isTests = true;
+    }
+
+    @BeforeEach
+    void loadFXML() {
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("main.fxml"));
+        try {
+            //Load the main.fxml file in the resources folder.
+            rootPane = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        BorderPane flowPane = ((BorderPane) rootPane.lookup("#inputFlowPane"));
+        VBox inputVBox = ((VBox) flowPane.getChildren().get(0));
+        listView = ((ListView<Task>) inputVBox.getChildren().get(11));
+    }
+
+    @Test
+    void test(){
+        assertEquals(1,1);
+    }
 }
