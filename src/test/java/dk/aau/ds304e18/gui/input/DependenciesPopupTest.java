1228b8b4bc7f898f0bf401d45896bc98e08c57c7
Milton <mlindo17@student.aau.dk>
2018-11-29 15:12:50 +0100

    hej

--- src/test/java/dk/aau/ds304e18/gui/input/DependenciesPopupTest.java
+++ src/test/java/dk/aau/ds304e18/gui/input/DependenciesPopupTest.java
@@ -9,21 +9,34 @@ import javafx.scene.layout.VBox;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DependenciesPopupTest {
+
+    static ListView listView;
 
     @BeforeAll
     static void realInit() {
         DatabaseManager.isTests = true;
         Application.launch(StartUpTest.class);
+        BorderPane flowPane = ((BorderPane) StartUpTest.getRootPane().lookup("#inputFlowPane"));
+        VBox inputVBox = ((VBox) flowPane.getChildren().get(0));
+        listView = ((ListView<Task>) inputVBox.getChildren().get(11));
+    }
+
+    @Test
+    void dependenciesPopupConstructorTest01() {
+        DependenciesPopup test = new DependenciesPopup(StartUpTest.getRootPane(), listView);
+
+        assertEquals(test.getTaskDependencies(), new ArrayList<>());
     }
 
     @Test
     void openDependenciesPopupTest01() {
-        BorderPane flowPane = ((BorderPane) StartUpTest.getRootPane().lookup("#inputFlowPane"));
-        VBox inputVBox = ((VBox) flowPane.getChildren().get(0));
-        DependenciesPopup test = new DependenciesPopup(StartUpTest.getRootPane(), ((ListView<Task>) inputVBox.getChildren().get(11)));
+        DependenciesPopup test = new DependenciesPopup(StartUpTest.getRootPane(), listView);
         test.openDependenciesPopup();
         assertTrue(StartUpTest.getRootPane().getChildrenUnmodifiable().get(3).isVisible());
     }
